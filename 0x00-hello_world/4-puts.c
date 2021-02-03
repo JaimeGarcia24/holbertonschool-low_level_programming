44035dc23b985e5ac34071d2bf660a30bdc041c4
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-02-03 21:04:46 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char test[60] = "\"Programming is like building a multilingual puzzle";
+puts(test);
+return (0);
+}
