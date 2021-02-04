43c1bc991dfa140488f3f91f09723665f2af35ed
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-02-04 21:41:55 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Print numbers 0-9
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+int NB;
+
+for (NB = 0; NB <= 9; NB++)
+putchar(NB + '0');
+putchar('\n');
+return (0);
+}
