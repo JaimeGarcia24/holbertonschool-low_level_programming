e2882bb704e6cf7f880cd7ef7167b9e453b222b8
Jaime garcia <62820685+JaimeGarcia24@users.noreply.github.com>
2021-02-04 16:36:05 -0500

    NB PRINT

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,18 @@
-#include <stdlib.h>
+include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 
 /**
- * main - loop varibles
- * Return: 0 
+ * main - print numbers (0-9)
+ *
+ * Return: 0
  */
 int main(void)
 {
-char number = '0';
+char NB = '0';
 
-for (number = '0'; number <= '9'; number++)
-{
-putchar(number);
-}
+for (NB = '0'; NB <= '9'; NB++)
+putchar(NB);
 putchar('\n');
 return (0);
 }
