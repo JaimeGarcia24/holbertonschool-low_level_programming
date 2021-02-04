d517d4f57402e45b97ad42d56f3bed7856bc9ee7
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-02-04 22:04:01 +0000

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Numbers Hexadecimal
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char hexa;
+
+for (hexa = '0'; hexa <= '9'; hexa++)
+putchar(hexa);
+for (hexa = 'a'; hexa <= 'f'; hexa++)
+putchar(hexa);
+putchar('\n');
+return (0);
+}
