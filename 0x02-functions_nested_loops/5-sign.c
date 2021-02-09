968807317622cd8cf48673905f69e2220bf93afb
Jaime garcia <62820685+JaimeGarcia24@users.noreply.github.com>
2021-02-09 14:15:33 -0500

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,16 @@
 
 int print_sign(int n)
 {
-if(n > 0)
+if (n > 0)
 {
 _putchar('+');
-return (1);
-}else if( n < 0)
+return(1);
+}
+else if ( n < 0)
 {
 _putchar('-');
 return (-1);
-}else{
+} else{
 _putchar('0');
 return (0);
 }
