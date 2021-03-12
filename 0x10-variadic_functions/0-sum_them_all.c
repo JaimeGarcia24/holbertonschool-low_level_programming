2c6d15d5e067fba128d2e5a9023d13fe021c75e5
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-03-12 03:19:16 +0000

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,13 +10,16 @@ int sum_them_all(const unsigned int n, ...)
 {
 
 va_list sum;
+
 int c;
 unsigned int i;
 
 if (n == 0)
 return (0);
+
 c = 0;
 va_start(sum, n);
+
 for (i = 0; i < n; i++)
 {
 c += va_arg(sum, int);
